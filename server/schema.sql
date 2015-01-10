e25b2c5ad8a60bb5d9a7d0ef8f46d23dd55b67a4
cyancymk <cylee4@gmail.com>
2015-01-09 16:47:23 -0800

    Created basic db to start

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,83 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'users'
+--
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` INT(4),
+  `name` VARCHAR(255),
+  PRIMARY KEY (`id`)
 );
+
+-- ---
+-- Table 'messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` INT(4),
+  `id_users` INT(4),
+  `created_at` INT(10),
+  `message_text` VARCHAR(1000),
+  `roomname` VARCHAR(20),
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'friends'
+--
+-- ---
+
+DROP TABLE IF EXISTS `friends`;
+
+CREATE TABLE `friends` (
+  `id` INT(4),
+  `id_users` INT(4),
+  `id_friend` INT(4),
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+ALTER TABLE `friends` ADD FOREIGN KEY (id_users) REFERENCES `users` (`id`);
+ALTER TABLE `friends` ADD FOREIGN KEY (id_friend) REFERENCES `users` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `friends` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `users` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `messages` (`id`,`id_users`,`created_at`,`message_text`,`roomname`) VALUES
+-- ('','','','','');
+-- INSERT INTO `friends` (`id`,`id_users`,`id_friend`) VALUES
+-- ('','','');
+
 
 /* Create other tables and define schemas for them here! */
 
@@ -14,4 +88,3 @@ CREATE TABLE messages (
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
